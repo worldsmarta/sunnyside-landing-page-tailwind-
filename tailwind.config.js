b69ec3c0e8f6cb87b59bd01027f92ff08576a567
Smarta Dey <worldsmarta@gmail.com>
2021-09-30 07:21:34 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+
+      colors: {
+        'footer-col': '#91d3c5',
+        'footer-text': '#307767'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
